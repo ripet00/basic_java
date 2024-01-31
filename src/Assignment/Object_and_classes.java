e6dabb52fe3a0788a7b07b4354e3116fd6d725fe
Rifat <rifat.khaidir@gmail.com>
2024-01-31 20:28:00 +0800

    function

--- src/Assignment/Object_and_classes.java
+++ src/Assignment/Object_and_classes.java
@@ -1,11 +1,24 @@
 package Assignment;
 import java.util.Scanner;
 
-class Circle {
-    
+class circle {
+    int radius;
 }
+
+class rectangle {
+    int length, breadth;
+}
+
+class triangle {
+    int side_A, side_B, side_C;
+}
+
 public class Object_and_classes {
+    private static Scanner scan;
     public static void main(String[] args) {
-        
+        scan = new Scanner(System.in);
+
+        circle Circle = new circle();
+        System.out.print("");
     }
 }
---
+++ src/function/PasswordGenerator.java
@@ -0,0 +1,59 @@
+package function;
+import java.util.Scanner;
+import java.util.Random;
+
+public class PasswordGenerator {
+    private static Scanner scan;
+    private static final String LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL_CHARACTER = "!$%&|[](){}:;.,*+-#@<>~";
+
+    public static String generatePassword(int length, boolean lowerCase, boolean upperCase, boolean digits, boolean specialCharacter) {
+        StringBuilder password = new StringBuilder();
+        Random random = new Random();
+
+        String possibleCharacters = "";
+        if (lowerCase) {
+            possibleCharacters += LOWER_CASE;
+        }
+        if (upperCase) {
+            possibleCharacters += UPPER_CASE;
+        }
+        if (digits) {
+            possibleCharacters += DIGITS;
+        }
+        if (specialCharacter) {
+            possibleCharacters += SPECIAL_CHARACTER;
+        }
+
+        for (int i = 0 ; i< length; i++) {
+            int randomIndex = random.nextInt(possibleCharacters.length());
+            password.append(possibleCharacters.charAt(randomIndex));
+        }
+
+        return password.toString();
+    }
+    public static void main(String[] args) {
+        scan = new Scanner(System.in);
+
+        System.out.print("Enter the length of the password : ");
+        // Integer its to convert a string representation of a numarical value
+        // user can input string value become a boolean value (true or false)
+        int length = Integer.parseInt(scan.nextLine());
+        // int length = scan.nextInt();
+
+        System.out.print("Do you want to use lower case (y / n) : ");
+        boolean lowerCase = scan.nextLine().equalsIgnoreCase("y");
+        System.out.print("Do you want to use upper case (y / n) : ");
+        boolean upperCase = scan.nextLine().equalsIgnoreCase("y");
+        System.out.print("Do you want to use digits (y / n) : ");
+        boolean digits = scan.nextLine().equalsIgnoreCase("y");
+        System.out.print("Do you want to use special character (y / n) : ");
+        boolean specialCharacter = scan.nextLine().equalsIgnoreCase("y");
+
+        String password = generatePassword(length, lowerCase, upperCase, digits, specialCharacter);
+
+        System.out.println("Your password generated to : "+ password);
+    }
+}
